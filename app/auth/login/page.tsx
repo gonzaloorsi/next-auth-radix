55ed5a76a6d78aa680d82f06609205aeec5c7cc7
Gonzalo Orsi <gonzaloorsi@gmail.com>
2024-03-18 12:20:36 -0300

    first commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "@radix-ui/themes";
+import { LightningBoltIcon } from "@radix-ui/react-icons";
+
+export default function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <LightningBoltIcon width="16" height="16" />
+        Click
+      </Button>
+    </div>
+  );
+}
